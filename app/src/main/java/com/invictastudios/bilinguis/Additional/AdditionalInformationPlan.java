8ff611f99c64a89388851290a015803cc913ee03
Kristijan271197 <Kristijan271197@users.noreply.github.com>
2020-07-27 02:30:48 +0300

    ...

--- app/src/main/java/com/invictastudios/bilinguis/Additional/AdditionalInformationPlan.java
+++ app/src/main/java/com/invictastudios/bilinguis/Additional/AdditionalInformationPlan.java
@@ -20,7 +20,7 @@ public class AdditionalInformationPlan extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private PlanAdapter planAdapter;
-    private List<String> title;
+    private ArrayList<String> title;
     private List<String> description;
     private int level;
 
@@ -34,13 +34,13 @@ public class AdditionalInformationPlan extends AppCompatActivity {
         description = new ArrayList<>();
 
 
+
         if (bundle != null)
             level = bundle.getInt("level");
 
         if (level == 1) {
             for (int i = 0; i < 12; i++)
                 title.add("Day " + (i + 1));
-
             enterText("additional_info/A1/a1_plan_1.txt");
             enterText("additional_info/A1/a1_plan_2.txt");
             enterText("additional_info/A1/a1_plan_3.txt");
@@ -53,10 +53,10 @@ public class AdditionalInformationPlan extends AppCompatActivity {
             enterText("additional_info/A1/a1_plan_10.txt");
             enterText("additional_info/A1/a1_plan_11.txt");
             enterText("additional_info/A1/a1_plan_12.txt");
+            planAdapter = new PlanAdapter(this, title, description, 1);
         } else if (level == 2) {
             for (int i = 0; i < 15; i++)
                 title.add("Day " + (i + 1));
-
             enterText("additional_info/A2/a2_plan_1.txt");
             enterText("additional_info/A2/a2_plan_2.txt");
             enterText("additional_info/A2/a2_plan_3.txt");
@@ -72,10 +72,10 @@ public class AdditionalInformationPlan extends AppCompatActivity {
             enterText("additional_info/A2/a2_plan_13.txt");
             enterText("additional_info/A2/a2_plan_14.txt");
             enterText("additional_info/A2/a2_plan_15.txt");
+            planAdapter = new PlanAdapter(this, title, description, 2);
         }
 
         recyclerView = findViewById(R.id.additional_info_plan_recycler_view);
-        planAdapter = new PlanAdapter(this, title, description);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(planAdapter);
     }
--- app/src/main/java/com/invictastudios/bilinguis/adapters/PlanAdapter.java
+++ app/src/main/java/com/invictastudios/bilinguis/adapters/PlanAdapter.java
@@ -1,6 +1,7 @@
 package com.invictastudios.bilinguis.adapters;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +20,14 @@ public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.ViewHolder> {
     private Context context;
     private List<String> titles;
     private List<String> description;
-    private boolean[] checkBoxes;
+    int level;
 
-    public PlanAdapter(Context context, List<String> titles, List<String> description) {
+
+    public PlanAdapter(Context context, List<String> titles, List<String> description, int level) {
         this.context = context;
         this.titles = titles;
         this.description = description;
+        this.level = level;
     }
 
     @NonNull
@@ -37,8 +40,28 @@ public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
         holder.title.setText(titles.get(position));
         holder.description.setText(description.get(position));
+
+        holder.passedCheckBox.setOnClickListener(v -> {
+            editor.putBoolean("checkbox_" + level + "_" + position,
+                    !sharedPreferences.getBoolean("checkbox_" + level + "_" + position, false));
+            editor.apply();
+            getCheckBox(sharedPreferences, position, holder);
+        });
+        getCheckBox(sharedPreferences, position, holder);
+
+
+    }
+
+    private void getCheckBox(SharedPreferences sharedPreferences, int position, ViewHolder holder) {
+        if (sharedPreferences.getBoolean("checkbox_" + level + "_" + position, false))
+            holder.passedCheckBox.setChecked(true);
+        else
+            holder.passedCheckBox.setChecked(false);
     }
 
     @Override
